45de3c20fd1a7dec2506aa953e9cc49a3b0e9592
Penguinmaster800 <penguinmaster665@gmail.com>
2024-06-14 16:17:37 -0700

    Updating progress of level spawn
    *currently not working* posting progress of attempt to send chosen area data into level and spawn afterwards

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,5 +7,7 @@ var move_speed: int = 20
 #var path: PackedScene = preload(current_area)
 
 
-func _on_area_load(pos):
-	pass
+
+func _on_chosen_area(selected_arrangement):
+#	var loaded_area = selected_arrangement.instantiate() as Area2D
+	print(selected_arrangement)
