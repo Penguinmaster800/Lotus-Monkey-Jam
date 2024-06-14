555b083dd068dae9d33442307fe62ac99de9edf8
Penguinmaster800 <penguinmaster665@gmail.com>
2024-06-14 15:10:33 -0700

    Added Random Area Selection
    added area 1 selection, connected it to level with level as parent. Added additional element to status to indicate current level.

---
+++ scenes/levels/area1.gd
@@ -0,0 +1,14 @@
+extends LevelParent
+
+#list of area 1 obstacle arrangements
+var arrangement_1: PackedScene = preload("res://scenes/levels/area 1/arrangement_1.tscn")
+var arrangement_2: PackedScene = preload("res://scenes/levels/area 1/arrangement_2.tscn")
+var arrangement_3: PackedScene = preload("res://scenes/levels/area 1/arrangement_3.tscn")
+
+#selection of which arrangement to load
+var layout_options := [arrangement_1, arrangement_2, arrangement_3]
+var selected_arrangement = layout_options[randi()%len(layout_options)]
+
+
+
+
