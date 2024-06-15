b7b96f0ce156e34ab91ba96bd7cf1cb842fd6c11
Penguinmaster800 <penguinmaster665@gmail.com>
2024-06-15 12:43:09 -0700

    Emit progress status
    emit progress of monkee to use in other scenes, also update area

--- globals/status.gd
+++ globals/status.gd
@@ -1,6 +1,7 @@
 extends Node
 #emits signal intended for UI to update whenever needed
 signal stat_change
+signal progress_change
 
 #variables of  player stats
 var lives: int = 5:
@@ -12,6 +13,12 @@ var lives: int = 5:
 var stamina: int = 100:
 	set(value):
 		stamina = value
+		stat_change.emit()
+
+var progress: int = 10:
+	set(value):
+		progress = value
+		progress_change.emit()
 		stat_change.emit()
 
 #current area for spawn list
--- scenes/levels/main_menu.gd
+++ scenes/levels/main_menu.gd
@@ -1,8 +1,5 @@
 extends MarginContainer
 
 
-
-
-
 func _on_texture_button_pressed():
 	LoadingTransition.change_scene("res://scenes/levels/area1.tscn")
