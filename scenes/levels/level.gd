14f5938a252ff0f489adf5358d09592a235e19ae
Torey Clark <clarktorey+github@gmail.com>
2024-06-15 22:39:47 -0500

    Get level speed from global

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -20,6 +20,7 @@ var progress_so_far = 10
 
 
 func _ready():
+	move_speed = Status.speed
 	# Set the target for the level to be far to the left so it appears like the
 	#  the player is moving to the right
 	target = Vector2(position.x - level_length, position.y)
