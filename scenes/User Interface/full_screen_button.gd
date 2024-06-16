f8fd9130fe428fe3a039da274fcd79eaf7aacf74
Torey Clark <clarktorey+github@gmail.com>
2024-06-16 17:18:44 -0500

    Added full screen functionality

---
+++ scenes/User Interface/full_screen_button.gd
@@ -0,0 +1,15 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
