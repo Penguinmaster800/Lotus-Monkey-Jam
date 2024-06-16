6608ead6b2e1272902e8fc19b4a5ab8954f638ba
Torey Clark <clarktorey+github@gmail.com>
2024-06-15 23:27:55 -0500

    Worked on stuff
    - Started adding texture to the stamina bar
    - Attached health state to heath bar
    - Added hover state texture to quit button

--- scenes/User Interface/HUD.gd
+++ scenes/User Interface/HUD.gd
@@ -5,12 +5,8 @@ func _ready():
 	Status.stat_change.connect(_on_status_stat_change)
 	Status.progress_change.connect(_on_progress_change)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_status_stat_change():
-	$Stamina/ProgressBar.value = Status.stamina
+	$Stamina/ProgressBar.value = 5 / Status.lives * 100
 
 func _on_progress_change():
 	$LevelProgress/ProgressBar.value = 10 - Status.progress
