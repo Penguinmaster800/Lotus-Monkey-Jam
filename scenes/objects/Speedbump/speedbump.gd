43ef6f205dd8668a5c02b46f3d68d89d8e945b11
Torey Clark <clarktorey+github@gmail.com>
2024-06-16 17:48:29 -0500

    Corrected some obstacles

--- scenes/objects/Speedbump/speedbump.gd
+++ scenes/objects/Speedbump/speedbump.gd
@@ -1,14 +1,8 @@
 extends Area2D
 
-
-
-
-
 func _on_body_entered(body):
 	Status.stamina -= 10
 	Status.speed += 35
-
-
 
 func _on_body_exited(body):
 	Status.speed -= 35 # Replace with function body.
